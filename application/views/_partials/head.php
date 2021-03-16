cfa4e502e2692d2770191b705ebf23ac5d8a8d63
rahmat gibran zulbadriansyah <rgibran234@gmail.com>
2021-03-16 23:00:40 +0700

    revert tambah assets template

--- application/views/_partials/head.php
+++ application/views/_partials/head.php
@@ -4,6 +4,3 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
 <link rel="stylesheet" href="<?php echo base_url('assets/css/dataTables.bootstrap4.min.css')?>">
 <link rel="stylesheet" href="<?php echo base_url('assets/css/jquery.dataTables.css') ?>">
-<link href="<?php echo base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet">
-<link href="<?php echo base_url('assets/css/datepicker3.css')?>" rel="stylesheet">
-<link href="<?php echo base_url('assets/css/styles.css')?>" rel="stylesheet">
--- application/views/_partials/script.php
+++ application/views/_partials/script.php
@@ -8,5 +8,3 @@
 <script type="text/javascript" src="<?php echo base_url('assets/js/vfs_fonts.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/buttons.html5.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/buttons.print.min.js') ?>"></script>
-<script src="<?php echo base_url('assets/js/bootstrap-datepicker.js')?>"></script>
-<script src="<?php echo base_url('assets/js/custom.js')?>"></script>
