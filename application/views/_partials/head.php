f2506ac942c69f8c943a07d78a5f5ac75f5db5b7
rahmat gibran zulbadriansyah <rgibran234@gmail.com>
2021-03-16 23:47:10 +0700

    tambah assets sb admin

--- application/views/_partials/head.php
+++ application/views/_partials/head.php
@@ -4,3 +4,6 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
 <link rel="stylesheet" href="<?php echo base_url('assets/css/dataTables.bootstrap4.min.css')?>">
 <link rel="stylesheet" href="<?php echo base_url('assets/css/jquery.dataTables.css') ?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
+<link href="<?php echo base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
--- application/views/_partials/script.php
+++ application/views/_partials/script.php
@@ -1,6 +1,9 @@
 <script src="<?php echo base_url('assets/js/jquery-3.5.1.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/bootstrap.bundle.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>  
+<script src="<?php echo base_url('assets/js/sb-admin-2.js') ?>"></script>  
+<script src="<?php echo base_url('vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+
 <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.dataTables.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/dataTables.buttons.min.js') ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/jszip.min.js') ?>"></script>
