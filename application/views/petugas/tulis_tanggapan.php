86f106e5cb163c8b595ecc1b09f8aaf5e23479c4
rahmat gibran zulbadriansyah <rgibran234@gmail.com>
2021-03-17 14:10:26 +0700

    view petugas dengan template

--- application/views/petugas/tulis_tanggapan.php
+++ application/views/petugas/tulis_tanggapan.php
@@ -1,79 +1,121 @@
- <!DOCTYPE html>
+
+<!DOCTYPE html>
 <html lang="en">
+	<?php $this->load->view('_partials/head.php') ?>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php $this->load->view('_partials/head') ?>
+
 </head>
-<body>
-	<?php $this->load->view('_partials/navbar') ?>
-	
-	<div class="container">
-		<div class="row my-5">
-			<div class="col-lg-12 mx-auto text-center">
-				<?php foreach ($pengaduan as $data): ?>
-				<img src="<?php echo base_url('assets/upload/images/').$data->foto; ?>" alt="..." width="70%" class="mx-auto">
-				<?php $id = $data->id_pengaduan; ?>
-				<div>
-					<h3 >pengaduan</h3>
-					<p><?php echo $data->tgl_pengaduan ?></p>
-				</div>
 
-				<div>
-					<p>
-						<?php echo $data->isi_laporan ?>
-					</p>
-				</div>
-				<?php endforeach ?>
-			</div>	
-		</div>
+<body id="page-top">
 
-		<?php foreach ($tanggapan as $data): ?>
-		<div class="row my-5">
-			<div class="col-lg-12 mx-auto">
-				
-				
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+    	<?php $this->load->view('_partials/sidebar.php') ?>
+        
 
-				<div class="card">
-				  
-				  <div class="card-header">
-				    <h6>Tanggapan</h6>
-				    <span><?php echo $data->tgl_tanggapan ?></span>
-				  </div>
-				  <div class="card-body">
-				    <p class="card-text"><?php echo $data->tanggapan ?></p>
-				  </div>
-				</div>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+        	<?php $this->load->view('_partials/navbar.php') ?>
+            <!-- Main Content -->
+            <div id="content">
 
-				
-			</div>	
-		</div>
-		<?php endforeach ?>
+                
 
-		<div class="row">
-			<div class="col-lg-12 mx-auto">
-				<center>
-					<h3>Tulis Tanggapan</h3>
-				</center>
-				<form action="<?php echo site_url('petugas/save_tanggapan') ?>" method="post" enctype='multipart/form-data'>
-                    <input type="hidden" value="<?php echo $id; ?>" name="id">
-		            <div class="form-group">
-		                    <!-- <label for="isi">isi laporan</label> -->
-		                    <textarea class="form-control" name="isi" id="isi"></textarea>
-		            </div>
-		     
-                    <input class="form-control btn btn-success" type="submit" value="Kirim Data">
-                </form>
-			</div>
-		</div>
-	</div>
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
 
-	<?php $this->load->view('_partials/script') ?>
-	<script src="//cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Halaman Tanggapan</h1>
+                    </div>
+
+                    <div class="row">
+                    	<?php foreach ($pengaduan as $data): ?>
+                    	
+						<div class="col-lg-12 mx-auto">
+							<div class="card shadow mb-4">
+								<img src="<?php echo base_url('assets/upload/images/').$data->foto; ?>" alt="..." width="70%" class="card-img-top">
+								<?php $id = $data->id_pengaduan; ?>
+                                <div class="card-header">
+                                    <h6 class="m-0 font-weight-bold text-primary">Pengaduan</h6>
+				    				<span class="small"><?php echo $data->tgl_pengaduan ?></span>
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text"><?php echo $data->isi_laporan ?></p>
+                                </div>
+                            </div>
+						</div>
+						<?php endforeach ?>
+					</div>
+
+                    <?php foreach ($tanggapan as $data): ?>
+	                    <div class="row">
+
+							<div class="col-lg-12">
+								<div class="card shadow mb-4">
+	                                <div class="card-header">
+	                                    <h6 class="m-0 font-weight-bold text-primary">Tanggapan</h6>
+					    				<span class="small"><?php echo $data->tgl_tanggapan ?></span>
+	                                </div>
+	                                <div class="card-body">
+	                                    <p class="card-text"><?php echo $data->tanggapan ?></p>
+	                                </div>
+	                            </div>
+							</div>
+
+						</div>
+					<?php endforeach ?>
+
+
+					<div class="row">
+						<div class="col-lg-12 mx-auto">
+							<center>
+								<h3>Tulis Tanggapan</h3>
+							</center>
+							<form action="<?php echo site_url('petugas/save_tanggapan') ?>" method="post" enctype='multipart/form-data'>
+			                    <input type="hidden" value="<?php echo $id; ?>" name="id">
+					            <div class="form-group">
+					                    <!-- <label for="isi">isi laporan</label> -->
+					                    <textarea class="form-control" name="isi" id="isi"></textarea>
+					            </div>
+					     
+			                    <input class="form-control btn btn-success" type="submit" value="Kirim Data">
+			                </form>
+						</div>
+					</div>	
+                    
+
+                </div>
+
+                
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <?php $this->load->view('_partials/footer.php') ?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <?php $this->load->view('_partials/script.php') ?>
+    
+    <script src="//cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
     <script>
         // Replace the <textarea id="editor1"> with a CKEditor 4
         // instance, using default configuration.
         CKEDITOR.replace( 'isi' );
     </script>
 </body>
+
 </html>
--- application/views/petugas/utama.php
+++ application/views/petugas/utama.php
@@ -1,87 +1,142 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+	<?php $this->load->view('_partials/head.php') ?>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php $this->load->view('_partials/head') ?>
+
 </head>
-<body>
-	<?php $this->load->view('_partials/navbar') ?>
-	
-	<div class="container-fluid">
-		<div class="row my-5">
-			<div class="col-lg-3">
-				<div class="card">
-					<div class="card-header">
-						<h4>Petugas</h4>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+    	<?php $this->load->view('_partials/sidebar.php') ?>
+        
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+        	<?php $this->load->view('_partials/navbar.php') ?>
+            <!-- Main Content -->
+            <div id="content">
+
+                
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                    </div>
+
+                    <div class="row">
+
+						<div class="col-lg-3">
+							<div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Seluruh Pengaduan : <?php echo $pengaduan ?></h6>
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">Belum Di Proses: <?php echo $blm_di_proses; ?></p>
+			    					<p class="card-text">Di Proses : <?php echo $di_proses; ?></p>
+			    					<p class="card-text">Selesai : <?php echo $selesai; ?></p>
+                                </div>
+                            </div>
+						</div>
+
+						<div class="col-lg-9">
+							<div class="card shadow mb-4">
+		                        <div class="card-header py-3">
+		                            <h6 class="m-0 font-weight-bold text-primary float-left">List Pengaduan Masyarakat</h6>
+		                            
+		                        </div>
+		                        <div class="card-body">
+		                            <div class="table-responsive">
+		                                <table class="table table-bordered" id="masyarakat" width="100%" cellspacing="0">
+		                                    <thead>
+												<tr>
+													<th>ID</th>
+													<th>Tanggal</th>
+													<th>ISI</th>
+													<th>Foto</th>
+													<th>Status</th>
+													<th>Action</th>
+												</tr>
+											</thead>
+
+											<tbody>
+												<?php foreach ($list_pengaduan as $data): ?>
+													
+												
+												<tr>
+													<td><?php echo $data->id_pengaduan?></td>
+													<td><?php echo $data->tgl_pengaduan?></td>
+													<td><?php echo $data->isi_laporan?></td>
+													<td><img src="<?php echo base_url('assets/upload/images/').$data->foto?>" alt="" width="50"></td>
+													<td>
+														<?php if($data->status == '0'): ?>
+															<a href="<?php echo site_url('petugas/proses_pengaduan/').$data->id_pengaduan ?>" class="btn btn-secondary">Menunggu</a>
+														<?php elseif($data->status == 'proses'): ?>
+															<a href="<?php echo site_url('petugas/selesai_pengaduan/').$data->id_pengaduan ?>" class="btn btn-primary" disabled>Diproses</a>
+														<?php elseif($data->status == 'selesai'): ?>
+															<button class="btn btn-success" disabled>Selesai</button>
+
+														<?php endif ?>
+													</td>
+													<td>
+														<a href="<?php echo site_url('petugas/lihat_pengaduan/').$data->id_pengaduan ?>"class="btn btn-primary">lihat</a><br>
+													</td>
+													
+												</tr>
+
+												<?php endforeach ?>
+											</tbody>
+		                                </table>
+		                            </div>
+		                        </div>
+		                    </div>
+						</div>
 					</div>
 
-					<div class="card-body">
-						<h5 class="card-title">Seluruh Pengaduan : <?php echo $pengaduan ?></h5>
-    					<p class="card-text">Belum Di Proses: <?php echo $blm_di_proses; ?></p>
-    					<p class="card-text">Di Proses : <?php echo $di_proses; ?></p>
-    					<p class="card-text">Selesai : <?php echo $selesai; ?></p>
-    					
-					</div>
-				</div>
-			</div>
 
-			<div class="col-lg-9">
-				<table id="pengaduan" class="text-center">
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>Tanggal</th>
-							<th>ISI</th>
-							<th>Foto</th>
-							<th>Status</th>
-							<th>Action</th>
-						</tr>
-					</thead>
+                    
 
-					<tbody>
-						<?php foreach ($list_pengaduan as $data): ?>
-							
-						
-						<tr>
-							<td><?php echo $data->id_pengaduan?></td>
-							<td><?php echo $data->tgl_pengaduan?></td>
-							<td><?php echo $data->isi_laporan?></td>
-							<td><img src="<?php echo base_url('assets/upload/images/').$data->foto?>" alt="" width="50"></td>
-							<td>
-								<?php if($data->status == '0'): ?>
-									<a href="<?php echo site_url('petugas/proses_pengaduan/').$data->id_pengaduan ?>" class="btn btn-secondary">Menunggu</a>
-								<?php elseif($data->status == 'proses'): ?>
-									<a href="<?php echo site_url('petugas/selesai_pengaduan/').$data->id_pengaduan ?>" class="btn btn-primary" disabled>Diproses</a>
-								<?php elseif($data->status == 'selesai'): ?>
-									<button class="btn btn-success" disabled>Selesai</button>
+                </div>
 
-								<?php endif ?>
-							</td>
-							<td>
-								<a href="<?php echo site_url('petugas/lihat_pengaduan/').$data->id_pengaduan ?>"class="btn btn-primary">lihat</a><br>
-							</td>
-							
-						</tr>
+                <div class="row">
+                	
+                </div>
+                <!-- /.container-fluid -->
 
-						<?php endforeach ?>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
+            </div>
+            <!-- End of Main Content -->
 
-	<?php $this->load->view('_partials/script') ?>
-	<script>
-		
+            <!-- Footer -->
+            <?php $this->load->view('_partials/footer.php') ?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <?php $this->load->view('_partials/script.php') ?>
+    
+    <script>
 		$(document).ready(function() {
-	    $('#pengaduan').DataTable( {
-	        dom: 'Bfrtip',
-	        buttons: [
-	            'copy', 'csv', 'excel', 'pdf', 'print'
-	        ]
-	    } );
-	} );
+		    $('#masyarakat').DataTable( {
+		        dom: 'Bfrtip',
+		        buttons: [
+		            'copy', 'csv', 'excel', 'pdf', 'print'
+		        ]
+		    } );
+		} );
 	</script>
 </body>
+
 </html>
