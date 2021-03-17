ad22da123f4747392ed5fbc912539fd4c142d189
rahmat gibran zulbadriansyah <rgibran234@gmail.com>
2021-03-17 11:20:27 +0700

    tampilan login

--- application/views/login.php
+++ application/views/login.php
@@ -3,33 +3,52 @@
 <head>
     <?php $this->load->view('_partials/head') ?>
 </head>
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-3 mx-auto">
-                <div class="card mt-5">
-                        <div class="card-body">
-                        <h1 class="text-center">Silahkan Login</h1>
-                        <form action="<?php echo site_url('login/auth') ?>" method="post">
-                        <div class="form-group">
-                                <label for="username">Username</label>
-                                <input class="form-control" type="text" name="username" id="username" placeholder ="masukan username">
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-6 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Silahkan Login</h1>
+                                    </div>
+                                    <form action="<?php echo site_url('login/auth') ?>" method="post">
+                                        <div class="form-group">
+                                                <label for="username">Username</label>
+                                                <input class="form-control" type="text" name="username" id="username" placeholder ="masukan username">
+                                        </div>
+                                        <div class="form-group">
+                                                <label for="password">Password</label>
+                                                <input class="form-control" type="password" name="password" id="password" placeholder ="masukan password">
+                                        </div>
+                                                <input class="form-control btn btn-primary" type="submit" value="masuk">
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="<?php echo site_url('registrasi')?>">Registrasi</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                                <label for="password">Password</label>
-                                <input class="form-control" type="password" name="password" id="password" placeholder ="masukan password">
-                        </div>
-                                <input class="form-control btn btn-primary" type="submit" value="masuk">
-                            </form>
-                            <center>
-                                <a href="<?= site_url('registrasi') ?>">registrasi</a>
-                            </center>
-                        </div>
+                    </div>
                 </div>
+
             </div>
+
         </div>
+
     </div>
 
-    <?php $this->load->view('_partials/script') ?>
+    <?php $this->load->view('_partials/script.php') ?>
 </body>
 </html>
