e9c89e44f958279f68522566b71039176751ea3b
rahmat gibran zulbadriansyah <rgibran234@gmail.com>
2021-03-17 11:55:49 +0700

    halaman registrasi

--- application/views/registrasi.php
+++ application/views/registrasi.php
@@ -3,45 +3,64 @@
 <head>
     <?php $this->load->view('_partials/head') ?>
 </head>
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-10 mx-auto">
-                <div class="card mt-5">
-                        <div class="card-body">
-                        <h1 class="text-center">Registrasi Masyarakat</h1>
-                        <form action="<?php echo site_url('registrasi/register') ?>" method="post">
-                        <div class="form-group">
-                                <label for="nik">NIK</label>
-                                <input class="form-control" type="number" name="nik" id="nik" placeholder ="masukan nik">
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-6 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Halaman Registrasi</h1>
+                                    </div>
+                                    <form action="<?php echo site_url('registrasi/register') ?>" method="post">
+                                    <div class="form-group">
+                                            <label for="nik">NIK</label>
+                                            <input class="form-control" type="number" name="nik" id="nik" placeholder ="masukan nik">
+                                    </div>
+                                    <div class="form-group">
+                                            <label for="nama">Nama Lengkap</label>
+                                            <input class="form-control" type="text" name="nama" id="nama" placeholder ="masukan nama lengkap">
+                                    </div>
+                                    <div class="form-group">
+                                            <label for="username">Username</label>
+                                            <input class="form-control" type="text" name="username" id="username" placeholder ="masukan username">
+                                    </div>
+                                    <div class="form-group">
+                                            <label for="password">Password</label>
+                                            <input class="form-control" type="password" name="password" id="password" placeholder ="masukan password">
+                                    </div>
+                                    <div class="form-group">
+                                            <label for="telp">Nomor Telepon</label>
+                                            <input class="form-control" type="text" name="telp" id="telp" placeholder ="08123456789">
+                                    </div>
+                                            <input class="form-control btn btn-success" type="submit" value="registrasi">
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="<?php echo site_url('registrasi')?>">Registrasi</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                                <label for="nama">Nama Lengkap</label>
-                                <input class="form-control" type="text" name="nama" id="nama" placeholder ="masukan nama lengkap">
-                        </div>
-                        <div class="form-group">
-                                <label for="username">Username</label>
-                                <input class="form-control" type="text" name="username" id="username" placeholder ="masukan username">
-                        </div>
-                        <div class="form-group">
-                                <label for="password">Password</label>
-                                <input class="form-control" type="password" name="password" id="password" placeholder ="masukan password">
-                        </div>
-                        <div class="form-group">
-                                <label for="telp">Nomor Telepon</label>
-                                <input class="form-control" type="text" name="telp" id="telp" placeholder ="08123456789">
-                        </div>
-                                <input class="form-control btn btn-success" type="submit" value="registrasi">
-                            </form>
-                            <center>
-                                <!-- <a href="http://<?= $server ?>modul/registrasi/">registrasi</a> -->
-                            </center>
-                        </div>
+                    </div>
                 </div>
+
             </div>
+
         </div>
+
     </div>
 
-    <?php $this->load->view('_partials/script') ?>
+    <?php $this->load->view('_partials/script.php') ?>
 </body>
 </html>
